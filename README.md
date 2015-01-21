71c07e44ad6862cda70831c9a8343ac523e03d43
Viktor Benei <viktor.benei@gmail.com>
2015-01-21 21:13:52 +0100

    base project with spaces in everywhere

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ReadMe
+
+Purpose of this sample app: to include space character in every crucial part of the project, to test that our Xcode build and other tools can correctly work with space in dir name, project file name, scheme name, etc.
---
+++ a folder with spaces/BitriseSampleAppsiOS With Spaces/BitriseSampleAppsiOS With Spaces/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BitriseSampleAppsiOS With Spaces
+//
+//  Created by Viktor Benei on 1/21/15.
+//  Copyright (c) 2015 Bitrise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
